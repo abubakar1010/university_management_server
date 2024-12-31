6692657cb3edfc70a9b65311a01fbb57bc349a87
abu bakar <abubakar850772@gmail.com>
2025-01-01 01:33:01 +0600

    add global error handler and root route for API

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import path from 'path'
 import { notFoundRoute } from './app/utils/notFoundRoute'
+import globalErrorHandler from './app/middleware/globalErrorHandler'
+import { rootRoute } from './routes/route'
 
 const app = express()
 
@@ -10,13 +12,16 @@ app.use(express.json())
 app.use( express.static(path.join(__dirname,"../src/public")))
 app.use(express.urlencoded({extended: true, limit: "1000kb"}))
 
-app.patch("/user", (req, res) => {
-    console.log(req.body);
-    res.send(req.body)
+// route middleware
+
+app.use("/api/v1", rootRoute)
+
+app.get("/", (req, res) => {
+    res.send("Server is connected")
 })
 
 
-
 app.use(notFoundRoute)
+app.use(globalErrorHandler)
 
 export default app;

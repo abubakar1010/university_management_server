0d30f8c0fa285a07ea6fbd89d7d9c71c100cfeeb
abu bakar <abubakar850772@gmail.com>
2025-01-01 01:25:31 +0600

    refactor global error handler to include validation error handling and improve response structure

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -3,30 +3,37 @@
 import { ErrorRequestHandler } from "express";
 import ApiError from "../utils/ApiError";
 
-const globalErrorHandler:ErrorRequestHandler = (err,req, res, next) => {
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+	if (err instanceof ApiError) {
+		res.status(err.statusCode).json({
+			success: err.success,
+			statusCode: err.statusCode,
+			message: err.message,
+			details: "",
+			path: err.path,
+			type: err.type,
+			error: err.error,
+			stack: err.stack,
+		});
+	} else if (err.name === "ValidationError") {
+		res.status(400).json({
+			success: false,
+			statusCode: 400,
+			message: err._message,
+			details: err.message,
+			path: req.originalUrl,
+			type: err.name,
+			stack: err.stack,
+		});
+	} else {
+		res.status(500).json({
+			success: false,
+			statusCode: 500,
+			message: "An unexpected error occurred.",
+			error: err,
+			stack: err.stack,
+		});
+	}
+};
 
-    if(err instanceof ApiError){
-        res.status(err.statusCode).json({
-            success: err.success,
-            statusCode: err.statusCode,
-            message: err.message,
-            path: err.path,
-            type: err.type,
-            error: err.error,
-            stack: err.stack,
-            timestamp: new Date().toISOString(),
-        });
-    }else {
-        res.status(500).json({
-            success: false,
-            statusCode: 500,
-            message: "An unexpected error occurred.",
-            error: err,
-            stack: err.stack,
-            timestamp: new Date().toISOString(),
-        });
-    }
-
-}
-
-export default globalErrorHandler
+export default globalErrorHandler;

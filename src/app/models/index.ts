6c6a8868f7b109af2a6750437f4aa7cf7b320b7f
abu bakar <abubakar850772@gmail.com>
2025-01-01 13:32:32 +0600

    add sub schemas for full name, guardian, and address data structures

---
+++ src/app/models/index.ts
@@ -0,0 +1,98 @@
+import { Schema } from "mongoose";
+import { TAddress, TFullName, TGuardian } from "../types";
+
+const fullNameSchema = new Schema<TFullName>(
+	{
+		firstName: {
+            type: String,
+            trim: true,
+            required: [true, "first name is required"]
+        },
+		lastName: {
+            type: String,
+            trim: true,
+            required: [true, "last name is required"]
+        },
+	},
+	{
+		_id: false,
+	}
+);
+
+const guardianSchema = new Schema<TGuardian>(
+    {
+        fullName: fullNameSchema,
+        address: {
+            type: String,
+            required: [true, "address is required"]
+        },
+        occupation: {
+            type: String,
+            required: [true, "occupation is required"]
+        },
+        contact: {
+            type: [
+                {
+                    type: String,
+                    trim: true,
+                    required: [true, "Contact is required"]
+                }
+            ],
+            required: [true, "contact is required"]
+        }
+
+    },
+    {
+        _id: false
+    }
+)
+
+
+const addressSchema = new Schema<TAddress>(
+    {
+        presentAddress:{
+            type: String,
+            required: [true, "Present address is required"]
+        },
+        permanentAddress:{
+            type: String,
+            required: [true, "Permanent address is required"]
+        },
+
+    },
+    {
+        _id: false
+    }
+)
+// const guardianSchema = new Schema(
+//     {
+
+//     },
+//     {
+//         _id: false
+//     }
+// )
+// const guardianSchema = new Schema(
+//     {
+
+//     },
+//     {
+//         _id: false
+//     }
+// )
+// const guardianSchema = new Schema(
+//     {
+
+//     },
+//     {
+//         _id: false
+//     }
+// )
+
+
+export {
+    fullNameSchema,
+    guardianSchema,
+    addressSchema
+    
+}

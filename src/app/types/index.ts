4a1d76250aafd4c846a12488db7bf67307acf187
abu bakar <abubakar850772@gmail.com>
2025-01-02 16:12:20 +0600

    refactor: rename TStatus to TUserStatus and add TSemesterStatus and TDuration types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -7,7 +7,9 @@ export type TGender = "MALE" | "FEMALE";
 
 export type TUserRole = "STUDENT" | "ADMIN" | "FACULTY";
 
-export type TStatus = "PENDING" | "ACCEPTED" | "REJECTED" | "BLOCKED";
+export type TUserStatus = "PENDING" | "ACCEPTED" | "REJECTED" | "BLOCKED";
+
+export type TSemesterStatus = "UPCOMING" | "ONGOING" | "ENDED"
 
 export type TAddress = {
 	presentAddress: string;
@@ -15,13 +17,18 @@ export type TAddress = {
 };
 
 export type TSchedule = {
-    day: string;
-    time: string;
-}
+	day: string;
+	time: string;
+};
 
 export type TGuardian = {
 	fullName: TFullName;
 	address: string;
 	contact: string[];
 	occupation: string;
-}
+};
+
+export type TDuration = {
+	start: Date;
+	end: Date;
+};

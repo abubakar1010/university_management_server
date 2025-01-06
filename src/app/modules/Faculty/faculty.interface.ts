afd09b87b2cdf168451aa4c9e045f218ac84ada2
abu bakar <abubakar850772@gmail.com>
2025-01-06 16:24:09 +0600

    feat: implement faculty module with models, routes, controllers, and validation

--- src/app/modules/Faculty/faculty.interface.ts
+++ src/app/modules/Faculty/faculty.interface.ts
@@ -1,22 +1,42 @@
-import { Types } from "mongoose";
-import { TAddress, TFullName, TGender, TSchedule } from "../../types";
+import { Model, Types } from 'mongoose';
 
-export type TFaculty = {
-	userid: string;
-	user: Types.ObjectId;
-	fullname: TFullName;
-	designation: string;
-	dateOfBirth: Date;
-	gender: TGender;
-	contact: string[];
-	emergencyContact: string;
-    address: TAddress;
-    avatar: string;
-    academicFaculty: Types.ObjectId;
-    academicDepartment: Types.ObjectId;
-    courses: Types.ObjectId[];
-    schedule: (TSchedule & {course: Types.ObjectId})[]
+export type TGender = 'male' | 'female' | 'other';
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
 };
 
+export type TFaculty = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  academicDepartment: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  isDeleted: boolean;
+};
 
-
+export interface FacultyModel extends Model<TFaculty> {
+  // eslint-disable-next-line no-unused-vars
+  isUserExists(id: string): Promise<TFaculty | null>;
+}
--- src/app/modules/Faculty/faculty.model.ts
+++ src/app/modules/Faculty/faculty.model.ts
@@ -1,90 +1,135 @@
-import { model, Schema } from "mongoose";
-import { TFaculty } from "./faculty.interface";
-import { addressSchema, fullNameSchema, scheduleSchema } from "../../models";
-import { Gender } from "../../constant";
-import { validateObjectId } from "../../utils/ObjectValidator";
+import { Schema, model } from 'mongoose';
+import { BloodGroup, Gender } from './faculty.constant';
+import { FacultyModel, TFaculty, TUserName } from './faculty.interface';
 
-const facultySchema = new Schema<TFaculty>(
-	{
-		user: {
-			type: Schema.ObjectId,
-			ref: "User",
-			required: [true, "User reference is required"],
-			validate: {
-				validator: (objectid) => validateObjectId(objectid),
-				message: "Data type of {VALUE} must be mongodb objectId",
-			},
-		},
-		userid: {
-			type: String,
-			required: [true, "Userid is required"],
-		},
-		fullname: fullNameSchema,
-		designation: {
-			type: String,
-			required: [true, "Designation is required"],
-		},
-		dateOfBirth: {
-			type: Date,
-			required: [true, "Birth date is required"],
-		},
-		gender: {
-			type: String,
-			enum: {
-				values: Gender,
-				message: "{VALUE} is not valid ",
-			},
-			required: true,
-		},
-		contact: [
-			{
-				type: String,
-				required: [true, "Contact is required"],
-			},
-		],
-		emergencyContact: {
-			type: String,
-			required: [true, "Emergency contact is required"],
-		},
-		avatar: {
-			type: String,
-			required: [true, "Avatar is required"],
-		},
-		address: addressSchema,
-		schedule: scheduleSchema,
-		courses: [
-			{
-				type: Schema.ObjectId,
-				ref: "Course",
-				required: [true, "Course reference is required"],
-				validate: {
-					validator: (objectid) => validateObjectId(objectid),
-					message: "Data type of {VALUE} must be mongodb objectId",
-				},
-			},
-		],
-		academicDepartment: {
-			type: Schema.ObjectId,
-			ref: "AcademicDepartment",
-			required: [true, "Academic department reference is required"],
-			validate: {
-				validator: (objectid) => validateObjectId(objectid),
-				message: "Data type of {VALUE} must be mongodb objectId",
-			},
-		},
-		academicFaculty: {
-			type: Schema.ObjectId,
-			ref: "AcademicFaculty",
-			required: [true, "Academic faculty reference is required"],
-			validate: {
-				validator: (objectid) => validateObjectId(objectid),
-				message: "Data type of {VALUE} must be mongodb objectId",
-			},
-		},
-	},
-	{
-		timestamps: true,
-	}
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'First Name is required'],
+    trim: true,
+    maxlength: [20, 'Name can not be more than 20 characters'],
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, 'Last Name is required'],
+    maxlength: [20, 'Name can not be more than 20 characters'],
+  },
+});
+
+const facultySchema = new Schema<TFaculty, FacultyModel>(
+  {
+    id: {
+      type: String,
+      required: [true, 'ID is required'],
+      unique: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'User id is required'],
+      unique: true,
+      ref: 'User',
+    },
+    designation: {
+      type: String,
+      required: [true, 'Designation is required'],
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, 'Name is required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: Gender,
+        message: '{VALUE} is not a valid gender',
+      },
+      required: [true, 'Gender is required'],
+    },
+    dateOfBirth: { type: Date },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    contactNo: { type: String, required: [true, 'Contact number is required'] },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency contact number is required'],
+    },
+    bloogGroup: {
+      type: String,
+      enum: {
+        values: BloodGroup,
+        message: '{VALUE} is not a valid blood group',
+      },
+    },
+    presentAddress: {
+      type: String,
+      required: [true, 'Present address is required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent address is required'],
+    },
+    profileImg: { type: String, default: '' },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'Acadcemic Department is required'],
+      ref: 'AcademicDepartment',
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'Acadcemic Faculty is required'],
+      ref: 'AcademicFaculty',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  },
 );
 
-export const Faculty = model<TFaculty>("Faculty", facultySchema);
+// generating full name
+facultySchema.virtual('fullName').get(function () {
+  return (
+    this?.name?.firstName +
+    '' +
+    this?.name?.middleName +
+    '' +
+    this?.name?.lastName
+  );
+});
+
+// filter out deleted documents
+facultySchema.pre('find', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+facultySchema.pre('findOne', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+facultySchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
+//checking if user is already exist!
+facultySchema.statics.isUserExists = async function (id: string) {
+  const existingUser = await Faculty.findOne({ id });
+  return existingUser;
+};
+
+export const Faculty = model<TFaculty, FacultyModel>('Faculty', facultySchema);

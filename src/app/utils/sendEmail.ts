1ccead6139d36d6b8d3804e36384a60ec4023b7a
abu bakar <abubakar850772@gmail.com>
2025-01-06 16:41:02 +0600

    chore: add ESLint and Prettier configuration files, update package scripts, and create example environment file

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -7,14 +7,14 @@ export const sendEmail = async (to: string, html: string) => {
     port: 587,
     secure: config.NODE_ENV === 'production',
     auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: 'mezbaul@programming-hero.com',
+
+      user: 'abubakar850772@gmail.com',
       pass: 'xfqj dshz wdui ymtb',
     },
   });
 
   await transporter.sendMail({
-    from: 'mezbaul@programming-hero.com', // sender address
+    from: 'abubakar850772@gmail.com', // sender address
     to, // list of receivers
     subject: 'Reset your password within ten mins!', // Subject line
     text: '', // plain text body

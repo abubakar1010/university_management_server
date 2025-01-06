17cc32a47366742ac7c6828108f659b4591515dc
abu bakar <abubakar850772@gmail.com>
2025-01-06 16:22:19 +0600

    feat: implement admin module with CRUD operations, validation, and routes

--- src/app/modules/Admin/admin.interface.ts
+++ src/app/modules/Admin/admin.interface.ts
@@ -1,15 +1,40 @@
-import { Types } from "mongoose";
-import { TAddress, TFullName, TGender } from "../../types";
+import { Model, Types } from 'mongoose';
+
+export type TGender = 'male' | 'female' | 'other';
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
 
 export type TAdmin = {
-    userid: string;
-    user: Types.ObjectId;
-    fullname: TFullName;
-    dateOfBirth: Date;
-    gender: TGender;
-    contact: string[];
-    emergencyContact: string;
-    address: TAddress;
-    avatar: string;
-    
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  isDeleted: boolean;
 };
+
+export interface AdminModel extends Model<TAdmin> {
+  // eslint-disable-next-line no-unused-vars
+  isUserExists(id: string): Promise<TAdmin | null>;
+}
